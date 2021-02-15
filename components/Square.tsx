84c23a7039617b1b00428823106271ae389d81ba
mewiemers <m.wiemers01@gmail.com>
2021-02-15 16:48:10 +0100

    lifting state up

--- components/Square.tsx
+++ components/Square.tsx
@@ -1,14 +1,14 @@
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
 import styles from "../styles/Square.module.css";
 
 type Props = {
   value: ReactNode;
+  onClick?(): void;
 };
 export default function Square(props: Props) {
-  const [value, setValue] = useState(null);
   return (
-    <button className={styles.square} onClick={() => setValue("X")}>
-      {value}
+    <button className={styles.square} onClick={() => props.onClick()}>
+      {props.value}
     </button>
   );
 }
