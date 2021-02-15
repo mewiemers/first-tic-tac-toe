44df619887daf88c8499b453f4fec06a0b744910
mewiemers <m.wiemers01@gmail.com>
2021-02-15 18:45:55 +0100

    declaring a winner

--- components/Board.tsx
+++ components/Board.tsx
@@ -17,7 +17,15 @@ export default function Board() {
     return <Square value={squares[i]} onClick={() => handleClick(i)} />;
   }
 
-  const status = "Next player: " + (xIsNext ? "X" : "O");
+  // const status = "Next player: " + (xIsNext ? "X" : "O");
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
 
   return (
     <div>
@@ -39,4 +47,23 @@ export default function Board() {
       </div>
     </div>
   );
+}
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
